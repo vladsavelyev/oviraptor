634aaea8cf7e2bc7d8ef7254e146ee070f843ff6
vladsaveliev <vladislav.sav@gmail.com>
2018-09-03 13:40:24 +1000

    Add mosdepth-based script to check viral coverage

---
+++ check_cov.sh
@@ -0,0 +1,25 @@
+BAM=$1
+FAI=$2
+if [ -z $FAI ] ; then
+    FAI=/data/cephfs/punim0010/local/development/bcbio/genomes/Hsapiens/GRCh37/viral/gdc-viral.fa.fai
+fi
+
+PFX="${BAM%.*}"
+OUT=$PFX-completeness.txt
+
+# running mosdepth
+mosdepth -t 32 $PFX $BAM -n --thresholds 1,5,25 --by <(awk 'BEGIN {FS="\t"}; {print $1 FS "0" FS $2}' $FAI) 
+
+# output file header
+echo -e "virus\tsize\treads\tdepth\t1x\t5x\t25x" > $OUT
+
+# output file contents
+paste \
+    <(samtools idxstats $BAM | grep -v "\*" | sort -k1,1) \
+    <(zcat $PFX.regions.bed.gz | sort -k1,1) \
+    <(zgrep -v ^# $PFX.thresholds.bed.gz | sort -k1,1) \
+    | awk 'BEGIN {FS="\t"} { print $1 FS $2 FS $3 FS $8 FS $14/$2 FS $15/$2 FS $16/$2}' \
+    | sort -n -r -k6,6 -k5,5 >> $OUT
+
+# echo'ing output file name
+echo $OUT
