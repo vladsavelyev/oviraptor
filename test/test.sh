d1549b7fd62717cfcf939b2149cd52b887b5f9ee
vladsaveliev <vladislav.sav@gmail.com>
2020-10-05 22:20:14 +1100

    Test with BWA

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 test -e chr8.fa.gz || wget --no-verbose https://hgdownload.soe.ucsc.edu/goldenPath/hg38/chromosomes/chr8.fa.gz
-oviraptor tumor.bam -o results --host-fa chr8.fa.gz
+oviraptor tumor.bam -o results --host-fa chr8.fa.gz --aligner bwa
 
