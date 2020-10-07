6e9a9c13f24555b978113aed10db77f98ffccea5
vladsaveliev <vladislav.sav@gmail.com>
2020-10-07 14:55:29 +1100

    test.sh: bgzip chr8

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 test -e chr8.fa.gz || wget --no-verbose https://hgdownload.soe.ucsc.edu/goldenPath/hg38/chromosomes/chr8.fa.gz
+test -e chr8.fa.gz.gzi || gunzip chr8.fa.gz && bgzip chr8.fa && samtools faidx chr8.fa.gz
 oviraptor tumor.bam -o results --host-fa chr8.fa.gz
 
